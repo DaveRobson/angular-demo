15e2dd77611751c34ac8a22df0365b25fc49474a
David Robson <david.robson@bookitude.com>
2014-06-28 10:08:39 +0100

    basic working index jade
    Add the server routes structure and a basic jade page setup

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ var express = require('express'),
 	favicon = require('serve-favicon'),
 	morgan = require('morgan'),
 	bodyParser = require('body-parser'),
-	methodOverride = require('method-override');
+	methodOverride = require('method-override'),
+    path = require('path'),
+	http = require('http');
+
+	//server url routing
+	routes = require('./server/routes');
 
 
 var server = express();
-
 
 
 server.set('port', process.env.PORT || 3000);
@@ -24,8 +28,7 @@ server.use(methodOverride());
 server.use(express.static(path.join(__dirname, '/client/public')));
 
 
-server.get('/', 'index');
-
+server.get('/', routes.index);
 
 http.createServer(server).listen(server.get('port'), function(){
   console.log("Express server listening on port " + server.get('port'));
